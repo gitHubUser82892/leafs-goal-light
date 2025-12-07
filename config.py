e4d9a6c50edd449f37ab91c24378769c1705da84
Ryan Mayor <rmayor@gmail.com>
2025-12-07 11:20:41 -0500

    update speaker ip

--- config.py
+++ config.py
@@ -9,8 +9,8 @@ This is the single source of truth for IP addresses and other shared settings.
 # Sonos speaker IP addresses
 # Update these if your Sonos speakers get new DHCP addresses
 SONOS_OFFICE_IP = "192.168.86.29"      # Office:1 Sonos speaker
-SONOS_FAMILY_ROOM_IP = "192.168.86.36" # FamilyRoom2 speaker
-SONOS_BEAM_IP = "192.168.86.196"       # Family Room Beam Sonos speaker
+SONOS_FAMILY_ROOM_IP = "192.168.70.100" # FamilyRoom2 speaker
+SONOS_BEAM_IP = "192.168.70.101"       # Family Room Beam Sonos speaker
 
 # List of all Sonos IPs for easy iteration
 SONOS_SPEAKER_IPS = [
